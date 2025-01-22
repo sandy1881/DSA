1d7ee79dc9462d8fd4497db294273658150c70ac
Sandy <sandeshrnaik2000@gmail.com>
2025-01-22 14:38:38 +0530

    Doubly Linked List Push operation

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -12,7 +12,25 @@ class DoublyLinkedList{
     this.head = newNode;
     this.tail = newNode;
   }
+  
+  push(val){
+    const newNode = new Node(val);
+    if(!this.head){
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    else{
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+  }
+
 }
 
 const first = new DoublyLinkedList(10);
+first.push(20);
+first.push(30);
+first.push(40);
+first.push(50);
 console.log(first);
