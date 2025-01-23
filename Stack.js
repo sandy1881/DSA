8f5b0c34a2e6a66017bb75bc8beabc0352907f65
Sandy <sandeshrnaik2000@gmail.com>
2025-01-23 22:57:15 +0530

    Stack push Operation

--- Stack.js
+++ Stack.js
@@ -11,8 +11,25 @@ class Stack{
     this.top = newNode;
     this.length = 1;
   }
+
+  push(val){
+    const newNode = new Node(val);
+    if(this.length=== 0){
+      newNode = this.top;
+    }
+    else{
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
-let first = new Stack(1)
+let first = new Stack(1);
+first.push(2);
+first.push(3);
+first.push(4);
+first.push(5);
 
 console.log(first);
