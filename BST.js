4036a31d2b2a54eb7da6899bdc75afd4aa4fe43e
Sandy <sandeshrnaik2000@gmail.com>
2025-01-24 23:51:05 +0530

    BST constructor

---
+++ BST.js
@@ -0,0 +1,16 @@
+class Node{
+  constructor(val){
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST{
+  constructor(){
+    this.root = null;
+  }
+}
+
+let first = new BST();
+console.log(first);
